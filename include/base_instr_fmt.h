84c33efbb749e5ab92eab29dd27d9438607e251a
WaterDev-25 <matteo.bianchi@epitech.eu>
2025-03-21 17:06:30 +0100

    feat: rv32i implementation finished

--- include/base_instr_fmt.h
+++ include/base_instr_fmt.h
@@ -45,12 +45,24 @@ typedef struct {
     uint8_t _opcode : 7;
 } JType, *JTypePtr;
 
+typedef struct {
+    uint8_t _imm_12 : 1;
+    uint8_t _imm_10_5 : 6;
+    uint8_t _rs2 : 5;
+    uint8_t _rs1 : 5;
+    uint8_t _func3 : 3;
+    uint8_t _imm_4_1 : 4;
+    uint8_t _imm_11 : 1;
+    uint8_t _opcode : 7;
+} BType, *BTypePtr;
+
 typedef enum {
     INSTR_I_TYPE,
     INSTR_S_TYPE,
     INSTR_U_TYPE,
     INSTR_J_TYPE,
-    INSTR_R_TYPE
+    INSTR_R_TYPE,
+    INSTR_B_TYPE
 } InstrType, *InstrTypePtr;
 
 typedef struct {
@@ -62,6 +74,7 @@ typedef struct {
         UType _uType;
         JType _jType;
         RType _rType;
+        BType _bType;
     //} _format;
 } Instr, *InstrPtr;
 
--- src/disassembler.c
+++ src/disassembler.c
@@ -23,9 +23,7 @@ DisassemblerDisassemble(DisassemblerPtr disassemble)
                 disassemble->_section->_textOffset + offset + i];
         }
         nbr = *(uint32_t *)temp;
-        printf("%#010lx %#010x\n", disassemble->_section->_textOffset + offset, nbr);
-        printf("== Opcode: %#0x\n", nbr & 0x7F);
-        printf("== Func3: %#0x\n", (nbr >> 12) & 0x7);
+       // printf("%#010lx %#010x\n", disassemble->_section->_textOffset + offset, nbr);
         InstrNodePtr head = disassemble->_instrNode;
         while (head) {
             // I-Type encoding format check
@@ -34,7 +32,8 @@ DisassemblerDisassemble(DisassemblerPtr disassemble)
                 head->_instr._iType._imm = (nbr >> 20 & 0xFFF);
                 head->_instr._iType._rs1 = (nbr >> 15) & 0x1F;
                 head->_instr._iType._rd = (nbr >> 7) & 0x1F;
-                printf("== ITYPE INSTRUCTION FOUND!! %s x%d, x%d, %d\n",
+                printf("%#010lx\t%s x%d, x%d, %d\n",
+                    disassemble->_section->_textOffset + offset,
                     head->_instr._name, head->_instr._iType._rd,
                     head->_instr._iType._rs1,
                     head->_instr._iType._imm);
@@ -47,7 +46,8 @@ DisassemblerDisassemble(DisassemblerPtr disassemble)
                 head->_instr._sType._rs2 = (nbr >> 20) & 0x1F;
                 head->_instr._sType._imm_11_5 = (nbr >> 25) & 0x7;
                 head->_instr._sType._imm_4_0 = (nbr >> 7) & 0x1F;
-                printf("== STYPE INSTRUCTION FOUND!! %s x%d, %d(x%d)\n",
+                printf("%#010lx\t%s x%d, %d(x%d)\n",
+                    disassemble->_section->_textOffset + offset,
                     head->_instr._name,
                     head->_instr._sType._rs2,
                     ((head->_instr._sType._imm_11_5 << 5) |
@@ -59,7 +59,8 @@ DisassemblerDisassemble(DisassemblerPtr disassemble)
             if (head->_instr._uType._opcode == (nbr & 0x7F)) {
                 head->_instr._uType._imm = (nbr >> 12) & 0xFFFFF;
                 head->_instr._uType._rd = (nbr >> 7) & 0x1F;
-                printf("== UTYPE INSTRUCTION FOUND!! %s x%d, %d\n",
+                printf("%#010lx\t%s x%d, %d\n",
+                    disassemble->_section->_textOffset + offset,
                     head->_instr._name,
                     head->_instr._uType._rd,
                     head->_instr._uType._imm);
@@ -72,7 +73,8 @@ DisassemblerDisassemble(DisassemblerPtr disassemble)
                 head->_instr._jType._imm_10_1 = (nbr >> 21) & 0x3FF; 
                 head->_instr._jType._imm_20 = (nbr >> 31) & 0x1;
                 head->_instr._jType._rd = (nbr >> 7) & 0x1F;
-                printf("== JTYPE INSTRUCTION FOUND!! %s x%d, %d\n",
+                printf("%#010lx\t%s x%d, %d\n",
+                    disassemble->_section->_textOffset + offset,
                     head->_instr._name,
                     head->_instr._jType._rd,
                     head->_instr._jType._imm_20 ? ((head->_instr._jType._imm_20 << 20) |
@@ -83,6 +85,7 @@ DisassemblerDisassemble(DisassemblerPtr disassemble)
                     (head->_instr._jType._imm_19_12 << 12) |
                     (head->_instr._jType._imm_11 << 11) |
                     (head->_instr._jType._imm_10_1 << 1)));
+                break;
             }
             // R-Type encoding format check
             if (head->_instr._rType._opcode == (nbr & 0x7F) &&
@@ -91,11 +94,18 @@ DisassemblerDisassemble(DisassemblerPtr disassemble)
                 head->_instr._rType._rs2 = (nbr >> 20) & 0x1F;
                 head->_instr._rType._rs1 = (nbr >> 15) & 0x1F;
                 head->_instr._rType._rd = (nbr >> 7) & 0x1F;
-                printf("== RTYPE INSTRUCTION FOUND!! %s x%d, x%d, x%d\n",
+                printf("%#010lx\t%s x%d, x%d, x%d\n",
+                    disassemble->_section->_textOffset + offset,
                     head->_instr._name,
                     head->_instr._rType._rd,
                     head->_instr._rType._rs1,
                     head->_instr._rType._rs2);
+                break;
+            }
+            // B-Type encoding format check
+            if (head->_instr._bType._opcode == (nbr & 0x7F) &&
+                head->_instr._bType._func3 == ((nbr >> 12) & 0x7)) {
+                break;
             }
             head = head->_next;
         }
@@ -108,13 +118,45 @@ static FNSTATUS
 RegisterRV32Instruction(DisassemblerPtr disassembler)
 {
     Instr instructions[] = {
-        { ._name = "addi", ._type = INSTR_I_TYPE, ._iType = { ._func3 = 0x0, ._opcode = 0x13 } },
-        { ._name = "sw", ._type = INSTR_S_TYPE, ._sType = { ._func3 = 0x2, ._opcode = 0x23 } },
+        // I TYPE
+        { ._name = "addi", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x13 } },
+        { ._name = "slti", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x13, ._func3 = 0x2 } },
+        { ._name = "sltiu", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x13, ._func3 = 0x3 } },
+        { ._name = "xori", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x13, ._func3 = 0x4 } },
+        { ._name = "ori", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x13, ._func3 = 0x5 } },
+        { ._name = "andi", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x13, ._func3 = 0x7 } },
+        { ._name = "jalr", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x67 } },
+        { ._name = "lb", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x3 } },
+        { ._name = "lh", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x3, ._func3 = 0x1 } },
+        { ._name = "lw", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x3, ._func3 = 0x2 } },
+        { ._name = "lbu", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x3, ._func3 = 0x3 } },
+        { ._name = "lhu", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x3, ._func3 = 0x4 } },
+        // S Type
+        { ._name = "sb", ._type = INSTR_S_TYPE, ._sType = { ._opcode = 0x23 } },
+        { ._name = "sh", ._type = INSTR_S_TYPE, ._sType = { ._opcode = 0x23, ._func3 = 0x1 } },
+        { ._name = "sw", ._type = INSTR_S_TYPE, ._sType = { ._opcode = 0x23, ._func3 = 0x2 } },
+        // U Type
         { ._name = "lui", ._type = INSTR_U_TYPE, ._uType = { ._opcode = 0x37 } },
         { ._name = "auipc", ._type = INSTR_U_TYPE, ._uType = { ._opcode = 0x17 } },
+        // J Type
         { ._name = "jal", ._type = INSTR_J_TYPE, ._jType = { ._opcode = 0x6F } },
-        { ._name = "jalr", ._type = INSTR_I_TYPE, ._iType = { ._opcode = 0x67 } },
-        { ._name = "add", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33 } }
+        // R Type
+        { ._name = "add", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33 } },
+        { ._name = "xor", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func3 = 0x4 } },
+        { ._name = "or", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func3 = 0x6 } },
+        { ._name = "sll", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func3 = 0x1 } },
+        { ._name = "slt", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func3 = 0x2 } },
+        { ._name = "srl", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func3 = 0x5 } },
+        { ._name = "sra", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func3 = 0x5, ._func7 = 0x20 } },
+        { ._name = "sltu", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func3 = 0x3 } },
+        { ._name = "sub", ._type = INSTR_R_TYPE, ._rType = { ._opcode = 0x33, ._func7 = 0x20 } },
+        // B Type
+        { ._name = "beq", ._type = INSTR_B_TYPE, ._bType = { ._opcode = 0x63 } },
+        { ._name = "bne", ._type = INSTR_B_TYPE, ._bType = { ._opcode = 0x63, ._func3 = 0x1 } },
+        { ._name = "blt", ._type = INSTR_B_TYPE, ._bType = { ._opcode = 0x63, ._func3 = 0x4 } },
+        { ._name = "bge", ._type = INSTR_B_TYPE, ._bType = { ._opcode = 0x63, ._func3 = 0x5 } },
+         { ._name = "bltu", ._type = INSTR_B_TYPE, ._bType = { ._opcode = 0x63, ._func3 = 0x6 } },
+         { ._name = "bgeu", ._type = INSTR_B_TYPE, ._bType = { ._opcode = 0x63, ._func3 = 0x7 } }
     };
 
     for (size_t i = 0; i < sizeof(instructions) / sizeof(instructions[0]); i++)
